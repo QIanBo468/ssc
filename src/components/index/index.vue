fc63372350fb9d1b2422a301e9884a582eeeb762
王兴波 <wqianbo468@163.com>
2019-10-25 15:06:17 +0800

    index

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -5,15 +5,6 @@
       <van-swipe-item v-for="item of adSwiper" :key="item.id">
         <img :src="item.adimg" alt />
       </van-swipe-item>
-      <!-- <van-swipe-item>
-        <img src="../../../static/images/swiper@3x.png" alt />
-      </van-swipe-item>
-      <van-swipe-item>
-        <img src="../../../static/images/swiper1@3x.png" alt />
-      </van-swipe-item>
-      <van-swipe-item>
-        <img src="../../../static/images/swiper@3x.png" alt />
-      </van-swipe-item> -->
 
       <div class="custom-indicator" slot="indicator">
         <span :class="current == 0 ?  indicator : ''"></span>
@@ -36,13 +27,52 @@
       <p>我的钱包</p>
     </div>
 
-<!-- 状态列表 -->
-       <van-swipe :loop="false" :width="53" @change="onChange" :show-indicators='false' class="moneyswiper">
-      <van-swipe-item class="money-item" v-for="item of moneySwiper" :key="item.id">
-        <img :src="item.zuimg" alt />
-        <span>{{item.zuname}}</span>
+    <!-- 状态列表 -->
+    <van-swipe
+      :loop="false"
+      :width="330"
+      @change="onChange"
+      :show-indicators="false"
+      class="moneyswiper"
+    >
+      <van-swipe-item>
+        <div class="money-item" v-for="item of moneySwiper.slice(0,4)" :key="item.id">
+          <img :src="item.zuimg" alt />
+          <span>{{item.zuname}}</span>
+        </div>
+      </van-swipe-item>
+      <van-swipe-item>
+        <div class="money-item" v-for="item of moneySwiper.slice(4, 6)" :key="item.id">
+          <img :src="item.zuimg" alt />
+          <span>{{item.zuname}}</span>
+        </div>
       </van-swipe-item>
     </van-swipe>
+
+
+    <!-- 列表 -->
+  <van-list
+  v-model="loading"
+  :finished="finished"
+  finished-text="没有更多了"
+  @load="onLoad"
+>
+  <div class="moneylist" v-for="(item, index) of list" :key="index">
+    <div class="list-left">
+      <div class="left-active">
+        <div class="red"></div>
+        {{item.active}}
+      </div>
+      <p>{{item.name}}</p>
+    </div>
+    <div class="list-right">
+      <p class="right-time">{{item.time}}</p>
+      <div class="purse">￥{{item.purse}}</div>
+    </div>
+    
+  </div>
+</van-list>
+
   </div>
 </template>
 
@@ -51,27 +81,53 @@ export default {
   data() {
     return {
       adSwiper: [
-        {id:0 , adimg: './static/images/swiper@3x.png'},
-        {id:1 , adimg: './static/images/swiper1@3x.png'},
-        {id:2 , adimg: './static/images/swiper@3x.png'},
-        {id:3 , adimg: './static/images/swiper1@3x.png'}
+        { id: 0, adimg: "./static/images/swiper@3x.png" },
+        { id: 1, adimg: "./static/images/swiper1@3x.png" },
+        { id: 2, adimg: "./static/images/swiper@3x.png" },
+        { id: 3, adimg: "./static/images/swiper1@3x.png" }
       ],
       moneySwiper: [
-        {id: 0 , zuimg:'./static/images/zujifen.png',zuname:'宏信积分'},
-        {id: 1 , zuimg:'./static/images/zuqianbao.png',zuname:'静态钱包'},
-        {id: 2, zuimg:'./static/images/zuqianbao.png',zuname:'动态钱包'},
-        {id: 3 , zuimg:'./static/images/zujihuo.png',zuname:'激活码管理'},
-        {id: 4 , zuimg:'./static/images/zubangzhu.png',zuname:'寻求帮助'},
-        {id: 5 , zuimg:'./static/images/zubangzhu.png',zuname:'寻求帮助'},
+        { id: 0, zuimg: "./static/images/zujifen.png", zuname: "宏信积分" },
+        { id: 1, zuimg: "./static/images/zuqianbao.png", zuname: "静态钱包" },
+        { id: 2, zuimg: "./static/images/zuqianbao.png", zuname: "动态钱包" },
+        { id: 3, zuimg: "./static/images/zujihuo.png", zuname: "激活码管理" },
+        { id: 4, zuimg: "./static/images/zubangzhu.png", zuname: "寻求帮助" },
+        { id: 5, zuimg: "./static/images/zubangzhu.png", zuname: "寻求帮助" }
       ],
       current: 0,
       indicator: "xuanzhong",
-      gonggao: "三周年奖励之特大喜讯"
+      gonggao: "三周年奖励之特大喜讯",
+      loading: false,
+      finished: false,
+      list: [
+        {active: '匹配中', time: '2019.10.11  10:10:10', name: '123456789', purse: '1000.00'},
+        {active: '匹配中', time: '2019.10.11  10:10:10', name: '123456789', purse: '1000.00'},
+        {active: '匹配中', time: '2019.10.11  10:10:10', name: '123456789', purse: '1000.00'},
+        {active: '匹配中', time: '2019.10.11  10:10:10', name: '123456789', purse: '1000.00'}
+      ]
     };
   },
   methods: {
     onChange(index) {
       this.current = index;
+    },
+    onLoad () {
+      setTimeout (() =>{
+        
+        for (let i =0 ; i<4; i++){
+          console.log(this.list[i])
+        }
+
+        // 加载状态结束
+      
+      this.loading = false 
+
+      // 数据全部加载完成
+      if (this.list.length >=4){
+        this.finished = true
+      }
+      },500)
+     
     }
   }
 };
@@ -152,16 +208,87 @@ export default {
     font-size: 14px;
   }
 }
-.moneyswiper{
+.moneyswiper {
   margin-left: 11px;
- 
-  .money-item{
-    margin-right: 30px;
-     overflow: hidden;
+  margin-bottom: 20px;
+  .van-swipe-item {
+    display: flex;
   }
-  img{
-    width: 53px;
-    
+  .money-item {
+    margin-right: 20px;
+    width: 63px;
+    //  overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    img {
+      width: 53px;
+      margin-bottom: 5px;
+    }
+    span {
+      font-size: 12px;
+      text-align: center;
+    }
+  }
+}
+
+.moneylist{
+  background: #ffddaa;
+  color: #26263c;
+  height: 80px;
+  margin-bottom: 5px;
+  padding: 0 11px;
+  margin: 10px 11px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 4px;
+  p{
+    margin: 0;
+    padding: 0;
+  }
+  .list-left{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    .left-active{
+      display: flex;
+      align-items: center;
+      .red{
+      background: #f00;
+      width: 7px;
+      height: 7px;
+      border-radius: 50%;
+      margin-right: 5px;
+      }
+      color: #0B0B1F;
+      font-size: 12px;
+    }
+    p{
+      font-size: 16px;
+      color: #000
+    }
+  }
+  .list-right{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: flex-end;
+    p{
+      margin: 0;
+      padding: 0;
+    }
+    .right-time{
+     color:  #956621;
+     font-size: 12px;
+    }
+    .purse{
+      color: #000;
+      font-weight: bold;
+    }
   }
 }
 </style>
--- src/components/my_centre/myindex.vue
+++ src/components/my_centre/myindex.vue
@@ -1,15 +1,182 @@
 <template>
-  <div>
-      wode
+  <div class="my_index">
+    <van-nav-bar class="mi_nav" title="我的"></van-nav-bar>
+    <div class="usetitle">
+      <div class="useatuo">
+        <img :src="usexg.auther" width="53px" height="53px" alt />
+        <div class="usename">
+          <span>{{usexg.name}}</span>
+          <p>用户编号:{{usexg.bianhao}}</p>
+        </div>
+      </div>
+      <div class="active">{{usexg.active}}</div>
+    </div>
+    <div class="indexitem">
+      <span>资料管理</span>
+      <van-grid :border="false" :column-num="3" class="ziliaoicon">
+        <van-grid-item  text="资料查看">
+          <van-image  width="30px" src="../../../static/my_index/Folder@3x.png" />
+          资料查看
+        </van-grid-item>
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/Folder@3x.png" />
+          资料修改
+        </van-grid-item>
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/lock_2@3x.png" />
+          密码修改
+        </van-grid-item>
+      </van-grid>
+    </div>
+    <div class="indexitem">
+          <span>我的钱包</span>
+      <van-grid :border="false" :column-num="5" class="ziliaoicon">
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/Ways_of_payment@3x.png" />
+          排单币
+        </van-grid-item>
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/Purse@3x.png" />
+          动态钱包
+        </van-grid-item>
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/Purse@3x.png" />
+          静态钱包
+        </van-grid-item>
+        <van-grid-item >
+          <van-image width="30px" src="../../../static/my_index/Сalendar@3x.png" />
+          激活码管理
+        </van-grid-item>
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/Page@3x.png" />
+          帮助
+        </van-grid-item>
+      </van-grid>
+    </div>
+    <div class="indexitem">
+          <span>财务管理</span>
+      <van-grid :border="false" :column-num="4" class="ziliaoicon">
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/Ways_of_payment@3x.png" />
+          财务明细
+        </van-grid-item>
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/15@3x.png" />
+          我的奖励
+        </van-grid-item>
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/16@3x.png" />
+          推荐奖励
+        </van-grid-item>
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/Purse@3x.png" />
+          激活码转账
+        </van-grid-item>
+      </van-grid>
+    </div>
+    <div class="indexitem">
+          <span>其他项目</span>
+      <van-grid :border="false" :column-num="3" class="ziliaoicon">
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/18@3x.png" />
+          团队管理
+        </van-grid-item>
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/17@3x.png" />
+          推广二维码
+        </van-grid-item>
+        <van-grid-item>
+          <van-image width="30px" src="../../../static/my_index/19@3x.png" />
+          公告
+        </van-grid-item>
+      </van-grid>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      usexg: {
+        name: "大林子",
+        bianhao: 1234567,
+        active: "会员",
+        auther: "./static/my_index/auther.png"
+      }
+    };
+  }
+};
 </script>
 
-<style>
-
+<style lang='less' scope>
+.my_index {
+  padding: 0 11px;
+  .mi_nav {
+    background: transparent;
+    .van-nav-bar__title {
+      color: #ffddaa;
+    }
+  }
+  .usetitle {
+    height: 80px;
+    background: #ffddaa;
+    border-radius: 3px;
+    margin-bottom: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .useatuo {
+      display: flex;
+      align-items: center;
+      height: 100%;
+      .usename {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        height: 53px;
+        p {
+          padding: 0;
+          margin: 0;
+          font-size: 12px;
+          color: #0b0b1f;
+        }
+        span {
+          padding: 0;
+          margin: 0;
+          font-size: 16px;
+          color: #0b0b1f;
+        }
+      }
+    }
+    .active {
+      background: #0b0b1f;
+      border-top-left-radius: 13px;
+      border-bottom-left-radius: 13px;
+      color: #ffddaa;
+      text-align: center;
+      font-size: 12px;
+      padding: 6px 12px;
+    }
+  }
+  .indexitem {
+    color: #ffddaa;
+    span{
+      color: #ffddaa;
+      display: inline-block;
+      font-size: 14px;
+      margin-bottom: 10px;
+    }
+    .ziliaoicon {
+      border: 0.5px solid #ffddaa;
+      height: 80px;
+    }
+    .van-grid-item__content {
+      background: transparent;
+      font-size: 8px;
+      box-sizing: border-box;
+      padding: 0;
+    }
+  }
+}
 </style>
