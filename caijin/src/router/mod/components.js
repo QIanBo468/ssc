286efd32c3074596b46d41352d8526d7c1dda0b2
王兴波 <wqianbo468@163.com>
2019-11-23 18:43:59 +0800

    1

--- caijin/src/router/mod/components.js
+++
@@ -1,9 +0,0 @@
-const components_files = require.context('@/components', true, /\.vue$/),
-    components = {};
-    // Be lightweight, powerful and easy to use
-components_files.keys().forEach(key => {
-    if (components_files(key).default.name != "tabbar")
-        components[key.replace(/(\.\/|\.vue)/g, '')] = components_files(key).default
-})
-
-export default components
